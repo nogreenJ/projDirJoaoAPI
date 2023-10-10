775dc5f47273267a9d84a0636d37fda4847b7dc7
João Luiz Daré Pinto <66640581+nogreenJ@users.noreply.github.com>
2023-10-10 09:51:42 -0300

    Update segurancaController.js

--- controllers/segurancaController.js
+++ controllers/segurancaController.js
@@ -6,7 +6,7 @@ const login = async (request, response) => {
     await autenticaUsuarioDB(request.body)
         .then(usuario => {
             const token = jwt.sign({ usuario }, process.env.SECRET, {
-                expiresIn: 300 // expira em 5 minutos
+                expiresIn: 3600 // expira em 5 minutos
             })
             return response.json({ auth: true, token: token });
         })
