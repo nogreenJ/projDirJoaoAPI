daf1c9f627aaf24e0213f68957af58f5e126e8c9
João Luiz Daré Pinto <66640581+nogreenJ@users.noreply.github.com>
2023-10-14 11:58:25 -0300

    Update diretorioUseCases.js

--- usecases/diretorioUseCases.js
+++ usecases/diretorioUseCases.js
@@ -22,7 +22,7 @@ const addDiretorioDB = async (body) => {
         const results = await pool.query(`INSERT INTO diretorios (nome, usuario, parent) 
             VALUES ($1, $2, $3)
             returning codigo, nome`,
-            [nome, usuario]);
+            [nome, usuario, parent]);
         const diretorio = results.rows[0];
         return new Diretorio(diretorio.codigo, diretorio.nome);
     } catch (err) {
