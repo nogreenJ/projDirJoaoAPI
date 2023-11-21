027f14e8f6a681382b578bb00cc1a7f0fba8b23e
João Luiz Daré Pinto <66640581+nogreenJ@users.noreply.github.com>
2023-11-21 08:25:11 -0300

    Update rotas.js

--- routes/rotas.js
+++ routes/rotas.js
@@ -4,11 +4,14 @@ const { rotasDiretorios } = require('./rotasDiretorios');
 const { rotasUsuarios } = require('./rotasUsuarios');
 
 const { login } = require('../controllers/segurancaController');
+const { addUsuario } = require('../controllers/usuarioController');
 
 const rotas = new Router();
 
 // rota para o login
 rotas.route('/login').post(login);
+
+rotasUsuarios.route('/cadastro').post(addUsuario);
 
 rotas.use(rotasDiretorios);
 rotas.use(rotasUsuarios);
