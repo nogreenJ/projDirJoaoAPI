27c00a7a141c4d34e593e748f130ff24dfdca835
João Luiz Daré Pinto <66640581+nogreenJ@users.noreply.github.com>
2023-11-27 11:06:29 -0300

    Update diretorioUseCases.js

--- usecases/diretorioUseCases.js
+++ usecases/diretorioUseCases.js
@@ -15,6 +15,7 @@ const getDiretoriosDB = async (codigo) => {
 
 const getDiretoriosArquivosDB = async (codigo) => {
     try {
+        console.log("getDiretoriosArquivosDB")
         let rowsDir, rowsArq;
         if (codigo) {
             rowsDir = await pool.query(`SELECT * FROM diretorios where usuario = $1 ORDER BY codigo`, [codigo]);
@@ -27,6 +28,8 @@ const getDiretoriosArquivosDB = async (codigo) => {
             rowsDir = rows.map((diretorio) => new Diretorio(diretorio.codigo, diretorio.nome, (diretorio.parent ? diretorio.parent : '')));
             rowsArq = rows.map((arquivo) => new Arquivo(arquivo.codigo, arquivo.nome, arquivo.formato, (arquivo.parent ? arquivo.parent : ''), arquivo.dono, arquivo.criptografia, arquivo.cid));
         }
+        console.log(rowsDir)
+        console.log(rowsArq)
         return rowsDir.concat(rowsArq);
     } catch (err) {
         throw "Erro: " + err;
